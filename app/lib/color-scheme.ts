4e2c038080b2f7a093c51cfa2db7d436ba337455
KirdesMF <cedgourville@gmail.com>
2025-04-27 02:35:46 +0200

    update serverFn cookie

--- app/lib/color-scheme.ts
+++ app/lib/color-scheme.ts
@@ -6,30 +6,24 @@ import { queryOptions } from "@tanstack/react-query";
 const SCHEME = ["dark", "light", "system"] as const;
 const ColorSchemeSchema = v.picklist(SCHEME);
 
-export function getColorScheme() {
-  const colorScheme = getCookie("color-scheme");
-  const scheme = v.parse(ColorSchemeSchema, colorScheme);
-  return scheme ?? "system";
-}
-
-export function setColorScheme(colorScheme: "system" | "light" | "dark") {
-  setCookie("color-scheme", colorScheme, {
-    path: "/",
-    sameSite: "lax",
-    secure: true,
-    httpOnly: true,
-  });
-}
-
 export const setColorSchemeFn = createServerFn({ method: "POST" })
   .validator(ColorSchemeSchema)
   .handler(({ data }) => {
-    setColorScheme(data);
+    setCookie("color-scheme", data, {
+      path: "/",
+      sameSite: "lax",
+      secure: true,
+      httpOnly: true,
+    });
   });
 
-export const getColorSchemeFn = createServerFn({ method: "GET" }).handler(() => {
-  return getColorScheme();
-});
+export const getColorSchemeFn = createServerFn({ method: "GET" })
+  .validator(() => {
+    const colorScheme = getCookie("color-scheme");
+    const scheme = v.parse(ColorSchemeSchema, colorScheme);
+    return scheme ?? "system";
+  })
+  .handler(({ data }) => data);
 
 export const colorSchemeQuery = queryOptions({
   queryKey: ["color-scheme"],
